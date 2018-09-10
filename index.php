e208897cdd2c74168cdaee77dab410532f8540c5
islamrakib <rakibislam@gmail.com>
2018-09-10 19:58:36 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+    // //arithmethic oparetor
+    /* $x = 50;
+    // $y = 25;
+    // echo "Result add :",$x + $y."<br>";
+    // echo "Result Sub : ",$x - $y."<br>";
+    // echo "Result Multi : ",$x * $y."<br>";
+    // echo "Result Div : ",$x / $y."<br>";
+
+    //conditional stetment
+    /*$age = 19;
+    if($age <= 17){
+        echo "You are child";
+    }
+    else{
+        echo "You are an adult!";
+    }*/
+
+    // $x = "50";
+    // if($x === 50){
+    //     echo "Equal";
+    // }
+    // else{
+    //     echo "Not equal";
+    // }
+
+
+        
+    // $x = 40;
+    // $y = 20;
+    // if($x<=50){
+    //     if($y>=26){
+    //         echo "y is greater than 18";
+    //     }
+    //     else{
+    //         echo "y is less than 18";
+    //     }
+    // }
+    // else{
+    //     echo "x is greater than 50";
+    // }
+
+    // $user ="admin";
+    // $pass = "1234sk";
+    // if($user === "admin"){
+    //     if($pass === "1234sk"){
+    //         echo "Welcome";
+    //     }
+    //     else{
+    //         echo "wrong password";
+    //     }
+    // }
+    // else{
+    //     echo "Login false";
+    // }
+
+    $student ="900";
+    $conseder = "true";
+    if($student <"900" || $conseder =="true"){
+            echo "You are Promote";
+    }
+    else{
+        echo "you are not welcome";
+    }
+
+
+
+?>
